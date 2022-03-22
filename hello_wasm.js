4ae3db6e1597928b9438be1dbb741e50301b05fe
Abhijit Biswas <84321034+Liftoff-Studios@users.noreply.github.com>
2022-03-22 19:36:10 +0530

    Update hello_wasm.js

--- hello_wasm.js
+++ hello_wasm.js
@@ -113,7 +113,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('binary', import.meta.url);
+        input = new URL('hello_wasm_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
